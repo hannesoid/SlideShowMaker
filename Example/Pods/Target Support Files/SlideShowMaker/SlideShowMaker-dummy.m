25d67fa7bff24c101a1bd4216033f0b36d28d106
Hannes Oud <hannes.oud.dev@gmail.com>
2020-03-28 18:42:29 +0100

    Move code to package folder, remove Pods structure

--- Example/Pods/Target Support Files/SlideShowMaker/SlideShowMaker-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_SlideShowMaker : NSObject
-@end
-@implementation PodsDummy_SlideShowMaker
-@end
--- Sources/SlideShowMaker/SlideShowMaker.swift
+++
@@ -1,3 +0,0 @@
-struct SlideShowMaker {
-    var text = "Hello, World!"
-}
